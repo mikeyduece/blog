299711f6a182e3bf6bad7db84f7ac6a84f989274
Mike Heft <mikeheft@gmail.com>
2020-03-19 16:10:50 +0000

    Add PostList component

---
+++ src/components/PostList.jsx
@@ -0,0 +1,15 @@
+// Dependencies
+import React, { Component } from 'react'
+// Components
+
+class PostList extends Component {
+  render() {
+    return(
+      <div>
+        PostList
+      </div>
+    )
+  }
+}
+
+export default PostList
