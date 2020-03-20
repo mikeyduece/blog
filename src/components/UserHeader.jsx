0ea2fe4f41782651b95a42107bcf05289f048ddd
Mike Heft <mikeheft@gmail.com>
2020-03-20 12:50:54 +0000

    Move #find functionality to mapStateToProps

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -11,11 +11,12 @@ class UserHeader extends Component {
   }
 
   render() {
-    const user = this.props.users.find(user => user.id === this.props.userId)
+    const user = this.props.user
 
     if (!user) {
       return null
     }
+
     return (
       <div className='header'>
         { user.name }
@@ -23,8 +24,8 @@ class UserHeader extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  return { users: state.users }
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) }
 }
 
 export default connect(
